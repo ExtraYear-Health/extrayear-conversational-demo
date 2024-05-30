d9479576eec68b41315927f2e9773c3d52c71b47
adrivelasco <adrivelasco.dev@gmail.com>
2024-05-30 15:35:34 -0300

    feat: add more options for voice prob threshold

--- app/components/InitialLoad.tsx
+++ app/components/InitialLoad.tsx
@@ -123,6 +123,12 @@ const UtteranceEndMsSelection = ({ value, onChange }: NumberSelectOptionsProps) 
 
 const VadProbThresholdSelection = ({ value, onChange }: NumberSelectOptionsProps) => {
   const options = [
+    { value: 0.1, label: '10%' },
+    { value: 0.2, label: '20%' },
+    { value: 0.3, label: '30%' },
+    { value: 0.4, label: '40%' },
+    { value: 0.5, label: '50%' },
+    { value: 0.6, label: '60%' },
     { value: 0.7, label: '70%' },
     { value: 0.8, label: '80%' },
     { value: 0.9, label: '90%' },
