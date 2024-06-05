0bb6152b262f7fa7e8649644c642ae4c5b0882c4
adrivelasco <adrivelasco.dev@gmail.com>
2024-06-05 14:55:48 -0300

    feat: custom hook to join consecutive transcripts done by the same user role

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+module.exports = createJestConfig(customJestConfig);
